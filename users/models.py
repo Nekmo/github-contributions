567af1dc4d08be3c8c54a209f14c7cdd0006d4db
Nekmo <contacto@nekmo.com>
2019-02-19 23:44:03 +0100

    User model.

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from jsonfield import JSONField
+
+
+class User(models.Model):
+    avatar_id = models.URLField(blank=True)
+    gravatar_id = models.URLField(blank=True)
+    type = models.CharField(max_length=24)
+    name = models.CharField(max_length=128, db_index=True)
+    company = models.CharField(max_length=128)
+    blog = models.URLField(blank=True)
+    location = models.CharField(max_length=192, blank=True)
+    email = models.EmailField(blank=True)
+    bio = models.TextField(blank=True)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    data = JSONField(default=dict)
